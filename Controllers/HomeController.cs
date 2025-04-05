25b4910cb0f8fcf8ba0ba7281f6b288ce83e9a9e
TumanovaNelly <2-man.off@mail.ru>
2025-04-05 12:47:33 +0300

    Переделана главная страница, улучшена файловая система

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using ChillScrabble.Models;
 
-namespace ChillScrabble.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            var game = new Game();
-            return View(game);
-        }
+namespace ChillScrabble.Controllers;
 
-        public IActionResult GamePage()
-        {
-            return View();
-        }
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var game = new Game();
+        return View(game);
     }
 }
