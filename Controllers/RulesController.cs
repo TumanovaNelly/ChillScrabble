bc2c10c58d329da12e61fa477218609831ca6e2e
TumanovaNelly <2-man.off@mail.ru>
2025-04-26 16:39:22 +0300

    Заверстана страница правил

---
+++ Controllers/RulesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChillScrabble.Controllers;
+
+public class RulesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
